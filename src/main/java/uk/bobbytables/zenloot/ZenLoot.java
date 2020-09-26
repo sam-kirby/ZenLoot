8c8cf9e7fac9e853a4930c1fb2d969230486b8be
Sam Kirby <sam.kirby94@hotmail.co.uk>
2020-09-26 11:33:38 +0100

    init

---
+++ src/main/java/uk/bobbytables/zenloot/ZenLoot.java
@@ -0,0 +1,13 @@
+package uk.bobbytables.zenloot;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = "zenloot",
+        name = "ZenLoot",
+        version = "@VERSION@",
+        dependencies = "required-after:crafttweaker;after:zenstages"
+)
+public class ZenLoot {
+    
+}
